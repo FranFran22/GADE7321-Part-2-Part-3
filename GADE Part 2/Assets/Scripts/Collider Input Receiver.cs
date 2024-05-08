b9beae4a6565bcc63f3c24a976e07047e937d8a4
Fran <ArgentumArcus.12@gmail.com>
2024-05-08 22:08:02 +0200

    updated document

--- GADE Part 2/Assets/Scripts/Collider Input Receiver.cs
+++ GADE Part 2/Assets/Scripts/Collider Input Receiver.cs
@@ -8,7 +8,7 @@ public class ColliderInputReceiver : InputReceiver
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) //not working
         {
             RaycastHit hit;
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
