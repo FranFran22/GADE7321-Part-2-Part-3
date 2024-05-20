3699d7ea2f13afe8d29f049533a77db96e6e2047
FranFran22 <ArgentumArcus.12@gmail.com>
2024-05-20 16:31:46 +0200

    added AI Movement class

---
+++ GADE Part 2/Assets/Scripts/AI classes/AI Movement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class AIMovement : MonoBehaviour
+{
+    private Path chosenPath;
+    private LinkedList<Vector2Int> chosenMoves = new LinkedList<Vector2Int>();
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
